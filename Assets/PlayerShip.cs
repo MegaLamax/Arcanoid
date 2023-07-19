28850e085e3de40a551dd50a888dbce8412bd40b
LamaTestQa <100977059+MegaLamax@users.noreply.github.com>
2023-07-18 23:44:36 -0300

    normalized

--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -22,23 +22,36 @@ public class PlayerShip : MonoBehaviour
         Vector2 input = new Vector2();
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
-        
-        Vector3 currentPos = transform.position;
 
-        currentPos.x += input.x * speed * Time.deltaTime;
-        currentPos.y += input.y * speed * Time.deltaTime;
-
-        if (currentPos.x < -wallDistanceX)
-            currentPos.x = -wallDistanceX;
+        Vector2 inputV = input.normalized;
         
-        if (currentPos.x > wallDistanceX)
-            currentPos.x = wallDistanceX;
+        Vector2 currentPos = transform.position;
 
-        if (currentPos.y > wallDistanceY)
-            currentPos.y = wallDistanceY;
-        
-        if (currentPos.y < -wallDistanceY)
-            currentPos.y = -wallDistanceY;
+        currentPos += inputV * speed * Time.deltaTime;
+
+        currentPos.x = Mathf.Clamp(currentPos.x, -wallDistanceX, wallDistanceX);
+
+        currentPos.y = Mathf.Clamp(currentPos.y, -wallDistanceY, wallDistanceY);
+        {
+            //Vector3 inputNormal = new Vector3(input.x, input.y, 0f).normalized;
+
+            //Vector3 currentPos = transform.position;
+
+            //currentPos.x += input.x * speed * Time.deltaTime;
+            //currentPos.y += input.y * speed * Time.deltaTime;
+
+            //if (currentPos.x < -wallDistanceX)
+            //    currentPos.x = -wallDistanceX;
+
+            //if (currentPos.x > wallDistanceX)
+            //    currentPos.x = wallDistanceX;
+
+            //if (currentPos.y > wallDistanceY)
+            //    currentPos.y = wallDistanceY;
+
+            //if (currentPos.y < -wallDistanceY)
+            //    currentPos.y = -wallDistanceY;
+        }
 
         transform.position = currentPos;
 
