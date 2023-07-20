4ba4dc1122dcf2473d0c3e552b5dbff20a82bcfa
LamaTestQa <100977059+MegaLamax@users.noreply.github.com>
2023-07-20 00:09:00 -0300

    MoveBall

--- Assets/Script/Ballmove.cs
+++ Assets/Script/Ballmove.cs
@@ -5,35 +5,51 @@ using UnityEngine;
 public class Ballmove : MonoBehaviour
 {
 
-    public float forceMagnitude = 5f;
+    //public float forceMagnitude = 5f;
+    public float Speed = 5f;
     public float Max = 3f;
     public float Min = 1f;
 
-    private Rigidbody2D rb;
+    private Vector2 DirectionMove;
+    private float NextMoveTime;
+
+    //private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        rb = GetComponent<Rigidbody2D>();
-        MoveBall();
+        NextMoveTime = Time.time + Random.Range(Min,Max);
+        MoveDirectionBall();
+        //rb = GetComponent<Rigidbody2D>();
+        //MoveBall();
 
     }
 
-    private void MoveBall()
+    private void MoveDirectionBall()
     {
-        Vector2 randomForce = Random.insideUnitCircle * forceMagnitude * Time.deltaTime;
-        rb.AddForce(randomForce, ForceMode2D.Impulse);
-
-        float interval = Random.Range(Min, Max);
-
-        Invoke("MoveBall",interval);
-
+        float randomAngle = Random.Range(0f, 360f);
+        DirectionMove = new Vector2(Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad));
     }
+    //private void MoveBall()
+    //{
+    //    Vector2 randomForce = Random.insideUnitCircle * forceMagnitude * Time.deltaTime;
+    //    rb.AddForce(randomForce, ForceMode2D.Impulse);
+
+    //    float interval = Random.Range(Min, Max);
+
+    //    Invoke("MoveBall",interval);
+
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time>=NextMoveTime)
+        {
+            MoveDirectionBall();
+            NextMoveTime = Time.time + Random.Range(Min, Max);
+        }
+
+        transform.Translate(DirectionMove * Speed * Time.deltaTime);
     }
 }
